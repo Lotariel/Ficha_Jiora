1bc28d606c16cc1a2add2c5081b91ca344a1ef8d
Guilherme WilliamGui <gui-w-lima@hotmail.com>
2022-10-10 13:11:53 -0300

    Alteração da tela do jogador

--- Ficha_Jiora/View/Ficha_do_Jogador.Designer.cs
+++ Ficha_Jiora/View/Ficha_do_Jogador.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(282, 102);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // Ficha_do_Jogador
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
             this.Name = "Ficha_do_Jogador";
             this.Text = "Ficha_do_Jogador";
             this.ResumeLayout(false);
@@ -53,7 +42,5 @@
         }
 
         #endregion
-
-        private Button button1;
     }
 }
